933a5ec88d61a52343907ae916424e77826e8858
Mert Kahraman <73400722+TheDerebeyi@users.noreply.github.com>
2022-05-08 19:18:56 +0300

    s

--- src/nesneYonelimliAnalizVeTasarimProje/SogutucuyuAc.java
+++ src/nesneYonelimliAnalizVeTasarimProje/SogutucuyuAc.java
@@ -4,6 +4,7 @@ public class SogutucuyuAc extends Islem{
 	
 	public void IslemYap() {
 		sogutucu.durumDegistir(Durumlar.Online);
+		System.out.println("Soðutucu baþarýyla açýldý.");
 	}
 
 	public SogutucuyuAc(ISogutucu sogutucu) {
