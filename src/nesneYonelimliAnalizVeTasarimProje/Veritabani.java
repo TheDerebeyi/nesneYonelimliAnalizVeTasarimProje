527767f30d40b496b4277b401e1ace5dd5d3fc62
Mert Kahraman <73400722+TheDerebeyi@users.noreply.github.com>
2022-05-09 21:02:17 +0300

    s

--- src/nesneYonelimliAnalizVeTasarimProje/Veritabani.java
+++ src/nesneYonelimliAnalizVeTasarimProje/Veritabani.java
@@ -4,52 +4,47 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Scanner;
+
 public class Veritabani {
+	static String dburl = "jdbc:postgresql://localhost:5432/nesneyonelimveritabani";
+	static Connection conn = null;
 
+	static void baglan() {
+		try {
+			conn = DriverManager.getConnection(dburl, "postgres", "123456");
+			if (conn != null)
+				System.out.println("Baglanti Gerceklesti");
+			else
+				System.out.println("Baglanti Basarisiz");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 
-    public class Veritabani {
+	}
 
-        static String dburl = "jdbc:postgresql://localhost:5432/nesneyonelimveritabani";
-        static Connection conn = null;
+	public boolean Kontrol(String KullaniciAdi, String Sifre) {
+		String sql = "SELECT * FROM \"public\".\"nesneyonelimveritabani\"";
+		String isim = "";
+		String sifre = "";
 
-        static void baglan() {
-            try {
-                conn = DriverManager.getConnection(dburl, "postgres", "123456");
-                if (conn != null)
-                    System.out.println("Baglanti Gerceklesti");
-                else
-                    System.out.println("Baglanti Basarisiz");
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
+		Connection conn = this.Baglan();
+		try {
+			Statement stmt = conn.createStatement();
+			ResultSet rs = stmt.executeQuery(sql);
 
-        }
-        public boolean Kontrol(String KullaniciAdi , String Sifre)
-        {
-            String sql= "SELECT * FROM \"public\".\"nesneyonelimveritabani\"";
-            String isim = "";
-            String sifre = "";
+			conn.close();
 
-            Connection conn=this.Baglan();
-            try{
-                Statement stmt = conn.createStatement();
-                ResultSet rs = stmt.executeQuery(sql);
+			while (rs.next()) {
+				isim = rs.getString("ad");
+				sifre = rs.getString("sifre");
+			}
+			rs.close();
+			stmt.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return isim.equals(KullaniciAdi) && sifre.equals(Sifre);
 
-                conn.close();
-
-                while(rs.next())
-                {
-                    isim = rs.getString("ad");
-                    sifre = rs.getString("sifre");
-                }
-                rs.close();
-                stmt.close();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            return isim.equals(KullaniciAdi) && sifre.equals(Sifre);
-
-        }
-    }
+	}
 
 }
